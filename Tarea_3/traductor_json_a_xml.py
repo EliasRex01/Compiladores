e02fa9eb3068c0f13a9b4f1ff5634028e7e72e6c
Rex <eliaszaracho93@gmail.com>
2026-05-26 16:07:29 -0300

    agrege base para leer archivos json

--- Tarea_3/traductor_json_a_xml.py
+++ Tarea_3/traductor_json_a_xml.py
@@ -1,3 +1,33 @@
-import re 
-import xml.etree.ElementTree as ET 
-from enum import Enum 
+from doctest import master
+import re
+from tracemalloc import start # Tabla Oficial de Tokens
+TOKEN_REGEX = [ 
+    ('L_CORCHETE', r'\['),
+    ('R_CORCHETE', r'\]'), ('L_LLAVE', r'\{'),
+    ('R_LLAVE', r'\}'), ('COMA', r','),
+    ('DOS_PUNTOS', r':'),
+    ('PR_TRUE', r'(true|TRUE)\b'),
+    ('PR_FALSE', r'(false|FALSE)\b'),
+    ('PR_NULL', r'(null|NULL)\b'),
+    # Expresión exacta solicitada para numeros científicos 
+    ('LITERAL_NUM', r'\d+(\.\d+)?([eE][+-]?\d+)?'), 
+    ('LITERAL_CADENA', r'"[^"]*"'),
+    ('NEWLINE', r'\n'), ('SKIP', r'[ \t\r]+') 
+]
+
+class Token: 
+    def __init__(self, type, value, line):
+        self.type: type 
+        self.value: value 
+        self.line: line
+    def __repr__(self): 
+        return f"{self.type}"
+
+def lex(source_code): 
+    tokens = []        
+    line_num = 1 
+    master_regex = '|'.join(f'(?P<{name}>{regex})' for name, regex in TOKEN_REGEX)
+    last_match_end = 0 
+
+    for match in re.finditer(master_regex, source_code):
+        start, end = match.span()
